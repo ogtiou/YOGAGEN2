976682e7ed1316ec208ec6fa7f385f1ce8d2bc14
Teo Raju <teoraju@Teo-MacBook-Pro.local>
2025-04-21 23:12:55 +0300

    alertofffix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header/header";
-import Alert from "./components/alert/alert";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
