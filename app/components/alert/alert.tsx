221804c3343136544d7e906bc6dca120303d5ebb
Teo Raju <teoraju@m5-mac.asetussivu>
2026-05-19 14:00:23 +0300

    luelisaa update

--- app/components/alert/alert.tsx
+++ app/components/alert/alert.tsx
@@ -14,18 +14,19 @@ export default function Alert() {
             <div className="bg-[#fcf6ec] gap-6 py-2 px-2 mx-1 lg:px-6 rounded-2xl backdrop-blur-lg flex items-center justify-between">
               <div className="flex flex-col items-start">
                 <p className="font-semibold text-sm md:text-base select-none pl-1">
-                  Alkaen <span className="font-black">1.3.2026</span> Yoga S7 tunnit pidetään Yoga Club tiloissa Veneentekijäntie 18 B 2. Nähdään matolla
+                  {/* Alkaen <span className="font-black">1.3.2026</span> Yoga S7 tunnit pidetään Yoga Club tiloissa Veneentekijäntie 18 B 2. Nähdään matolla */}
+                  Viikkotunnit toistaiseksi peruttu.
                 </p>
                 {/* <p className="pl-1 text-sm md:text-base">
                   Uudella studiolla käy vain uudet 2026 hankitut kortit.
                 </p> */}
               </div>
                 <div className="flex items-center justify-center gap-2 text-warm pointer-events-auto">
-                  {/* <Link onClick={() => setIsVisible(false)} href={'/luelisaa'} className="px-5 py-2 bg-black rounded-xl font-bold button1 hover3">
+                  <Link onClick={() => setIsVisible(false)} href={'/luelisaa'} className="px-5 py-2 bg-black rounded-xl font-bold button1 hover3">
                     <div className="flex items-center justify-center font-bold text-warm">
                       <p className="relative uppercase text-nowrap text-center">Lue lisää</p>
                     </div>
-                  </Link> */}
+                  </Link>
                   <p onClick={() => setIsVisible(false)} className="p-2 cursor-pointer bg-black rounded-xl button1">
                     <X/>
                   </p>
---
+++ app/luelisaa/page.tsx
@@ -0,0 +1,17 @@
+
+
+export default function Luelisaa() {
+
+  return(
+    <div className="overflow-hidden">
+      <div className="w-full h-screen bg-warm flex items-center justify-center flex-col relative">
+        <p className="w-[21rem] sm:w-[35rem] font-bold text-lg sm:text-3xl sm:leading-13"> 
+            Alkaen 15.5.2026 viikkotunnit studiolla peruttu. <br/> 
+            Tulossa live Zoom- pilates tunnit. <br/> Ohjelma tarkentuu kesäkuun aikana. <br/> 
+            Jää myös kuulolle kesän odotetuista äänimaljarentoutus- tunneista. <br/> Nähdään matolla.
+        </p>
+        {/* <Footer2/> */}
+      </div>
+    </div>
+  )
+}
